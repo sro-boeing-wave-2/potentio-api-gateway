59034c6726d4d50092d1e2d07c3b317955b8265a
Nishant Jain <nishantkumarajain@gmail.com>
2018-09-30 23:37:18 +0530

    Added logging

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -19,6 +19,11 @@ namespace Gateway
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConsole();
+                    logging.AddDebug(); 
+                })
                 .UseStartup<Startup>();
     }
 }
