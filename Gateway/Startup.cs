a3a9c9ad5a24ea0b9b982be3aa28a95ecf9445bf
Bhavin Patel <mailmebhav@gmail.com>
2018-10-01 10:49:22 +0530

    added validation

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -55,69 +55,69 @@ namespace Gateway
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCors("AppPolicy");
-            //app.Use(async (context, next) =>
-            //{
+            app.Use(async (context, next) =>
+            {
 
 
-            //   Console.WriteLine(context.Request.Path);
-            //   //if (context.Request.Query.TryGetValue("access_token", out var token1))
-            //   //{
-            //   //    context.Request.Headers.Add("Authorization", $"Bearer {token1}");
-            //   //}
-            //   if (context.Request.Path == "/auth/login" || context.Request.Path == "/auth/register" || context.Request.Path == "/" ||
-            //   //context.Request.Path == "/auth" || context.Request.Path == "/auth/email" || 
-            //   context.Request.Path == "/auth/logout" || /*context.Request.Path == "/auth/details" ||*/ context.Request.Path == "/home")
-            //   {
-            //       Console.WriteLine("Calling next middleware");
-            //       await next();
-            //   }
+                Console.WriteLine(context.Request.Path);
+                //if (context.Request.Query.TryGetValue("access_token", out var token1))
+                //{
+                //    context.Request.Headers.Add("Authorization", $"Bearer {token1}");
+                //}
+                if (context.Request.Path.ToString() == "/auth/login" || context.Request.Path.ToString() == "/auth/register" || context.Request.Path.ToString() == "/" ||
+                //context.Request.Path == "/auth" || context.Request.Path == "/auth/email" || 
+                context.Request.Path.ToString() == "/auth/logout" || /*context.Request.Path == "/auth/details" ||*/ context.Request.Path.ToString() == "/home")
+                {
+                    Console.WriteLine("Calling next middleware");
+                    await next();
+                }
 
-            //   //if (_context.Request.Query.TryGetValue("access_token", out var token))
-            //   //{
-            //   //    _context.Request.Headers.Add("Authorization", $"Bearer {token}");
-            //   //}
-            //   //await next.Invoke();
+                //if (_context.Request.Query.TryGetValue("access_token", out var token))
+                //{
+                //    _context.Request.Headers.Add("Authorization", $"Bearer {token}");
+                //}
+                //await next.Invoke();
 
-            //   Chilkat.Global global = new Chilkat.Global();
-            //   global.UnlockBundle("Anything for 30-day trail");
-            //   Chilkat.Jwt jwt = new Chilkat.Jwt();
+                Chilkat.Global global = new Chilkat.Global();
+                global.UnlockBundle("Anything for 30-day trail");
+                Chilkat.Jwt jwt = new Chilkat.Jwt();
 
-            //   using (var client = new ConsulClient())
-            //   {
-            //       string ConsulIP = Environment.GetEnvironmentVariable("MACHINE_LOCAL_IP4");
-            //       client.Config.Address = new Uri(Environment.GetEnvironmentVariable("MACHINE_LOCAL_IP4") + ":8500");
-            //       //client.Config.Address = new Uri("http://172.17.0.1:8500");
-            //       var getPair = await client.KV.Get("secretkey");
-            //       string token = context.Request.Cookies["UserLoginAPItoken"];
-            //       //string token = context.Request.Headers["Authorization"];
+                using (var client = new ConsulClient())
+                {
+                    string ConsulIP = Environment.GetEnvironmentVariable("MACHINE_LOCAL_IP4");
+                    client.Config.Address = new Uri(Environment.GetEnvironmentVariable("MACHINE_LOCAL_IP4") + ":8500");
+                    //client.Config.Address = new Uri("http://172.17.0.1:8500");
+                    var getPair = await client.KV.Get("secretkey");
+                    string token = context.Request.Cookies["UserLoginAPItoken"];
+                    //string token = context.Request.Headers["Authorization"];
 
-            //       Console.WriteLine(token + "\n");
-            //       if (token != null)
-            //       {
-            //           var x = token.Replace("Bearer ", "");
-            //           Console.WriteLine(x + "\n");
-            //           Rsa rsaPublicKey = new Rsa();
-            //           rsaPublicKey.ImportPublicKey(Encoding.UTF8.GetString(getPair.Response.Value));
+                    Console.WriteLine(token + "\n");
+                    if (token != null)
+                    {
+                        var x = token.Replace("Bearer ", "");
+                        Console.WriteLine(x + "\n");
+                        Rsa rsaPublicKey = new Rsa();
+                        rsaPublicKey.ImportPublicKey(Encoding.UTF8.GetString(getPair.Response.Value));
 
-            //           Console.WriteLine(Encoding.UTF8.GetString(getPair.Response.Value) + "\n");
+                        Console.WriteLine(Encoding.UTF8.GetString(getPair.Response.Value) + "\n");
 
-            //           var isTokenVerified = jwt.VerifyJwtPk(x, rsaPublicKey.ExportPublicKeyObj());
+                        var isTokenVerified = jwt.VerifyJwtPk(x, rsaPublicKey.ExportPublicKeyObj());
 
-            //           Console.WriteLine(rsaPublicKey.ExportPublicKeyObj() + "\n");
+                        Console.WriteLine(rsaPublicKey.ExportPublicKeyObj() + "\n");
 
-            //           Console.WriteLine(isTokenVerified + "\n");
-            //           if (isTokenVerified)
-            //           {
-            //               Console.WriteLine(isTokenVerified + "\n");
-            //               await next();
-            //           }
-            //           else
-            //           {
-            //               await context.Response.WriteAsync("Unauthorised");
-            //           }
-            //       }
-            //   }
-            //});
+                        Console.WriteLine(isTokenVerified + "\n");
+                        if (isTokenVerified)
+                        {
+                            Console.WriteLine(isTokenVerified + "\n");
+                            await next();
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync("Unauthorised");
+                        }
+                    }
+                }
+            });
             // app.UseOcelot().Wait();
             //app.Use(async (context, next) => { Console.WriteLine("Next middleware"); await next(); });
             app.UseWebSockets();
